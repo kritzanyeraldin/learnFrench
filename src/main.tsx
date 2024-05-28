9c273aacf0dd6deee0973e8407cbb3927bd8ab55
kritzanyeraldin <kritzan.sullca.e@uni.pe>
2024-05-28 02:16:04 -0500

    Create Design of statistics section

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "@mantine/core/styles.css";
+import "@mantine/charts/styles.css";
+import "@mantine/carousel/styles.css";
 import { MantineProvider } from "@mantine/core";
 import theme from "./theme";
 
