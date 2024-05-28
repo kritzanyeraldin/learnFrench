89a0eaa1d5dc3107c53443621d14a18f102d0326
kritzanyeraldin <kritzan.sullca.e@uni.pe>
2024-05-28 14:27:47 -0500

    -fixed code

--- src/routes/Internal/Statistics/index.tsx
+++ src/routes/Internal/Statistics/index.tsx
@@ -132,7 +132,7 @@ const data = [
   {
     level: "C1",
     progress: 10,
-
+  },
 ];
 
 const Statistics = () => {
@@ -200,17 +200,13 @@ const Statistics = () => {
                     dataKey="id"
                     fz="h1"
                     fillOpacity={1}
-
                     series={[{ name: "progress", color: "ToreaBay.5" }]}
-
                     // textColor="black"
 
                     // style={{fontWeight:}}
                     tickLine="xy"
                     gridAxis="xy"
-
                     textColor="ToreaBay.6"
-
                   ></BarChart>
                 </Box>
               </Card>
@@ -246,7 +242,6 @@ const Statistics = () => {
             textColor="ToreaBay.6"
           />
         </Card>
-
       </Box>
     </Flex>
   );
